58b318896d3f992065d0c21d6bb884e34f32aa38
Patrick Lorran Menezes <patrickalmenezes@gmail.com>
2024-07-23 18:35:37 -0300

    project created

---
+++ Saleswebmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Saleswebmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
