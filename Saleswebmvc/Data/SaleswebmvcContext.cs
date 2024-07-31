e0cbcbf5950a63ef32144e9b1eebb92e77b1b4d5
Patrick Lorran Menezes <patrickalmenezes@gmail.com>
2024-07-31 15:07:51 -0300

    Deparments created whith views and controller

---
+++ Saleswebmvc/Data/SaleswebmvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Saleswebmvc.Models;
+
+namespace Saleswebmvc.Data
+{
+    public class SaleswebmvcContext : DbContext
+    {
+        public SaleswebmvcContext (DbContextOptions<SaleswebmvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Saleswebmvc.Models.Department> Department { get; set; } = default!;
+    }
+}
--- Saleswebmvc/Program.cs
+++ Saleswebmvc/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Saleswebmvc.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SaleswebmvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SaleswebmvcContext") ?? throw new InvalidOperationException("Connection string 'SaleswebmvcContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
